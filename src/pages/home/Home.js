28e768bdaeebdf2a57fbee3a3da46891cba38646
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-02-11 20:07:27 +0500

    added boiler home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import CButton from '../../uiComponents/CButton/CButton';
 
 function Home() {
     return (
         <div className="home">
-            <p>home</p>
+            <div className="c-container">
+                <div className="first-container">
+                    <span>
+                        <p className="main-heading">The smartest jobsite in the <br />Middle East.</p>
+                        <p>People at the core of every interaction</p>
+                    </span>
+                    <CButton>View demo</CButton>
+                </div>
+                <div className="second-container ml-4">
+                    <img src={require('../../assets/images/banImg1.png')} alt="img1" />
+                </div>
+            </div>
         </div>
     );
 }
