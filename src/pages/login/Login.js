8f69242455a76a0712ee4dac11b6bfaedff36821
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-02-13 19:50:49 +0500

    login is updated with latest design

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,55 +1,93 @@
 import { Input, Form } from 'antd';
 import Checkbox from 'antd/lib/checkbox/Checkbox';
 import React from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import CButton from '../../uiComponents/shared/CButton/CButton';
 import * as Rules from '../../utils/rules';
+const userTypes = { SEEKER: "SEEKER", EMPLOYER: "EMPLOYER", AGENCY: "AGENCY" };
 function Login() {
+
+    const [userType, setUserType] = useState(userTypes.SEEKER);
     const onFinish = () => {
 
     }
+    const getIsActive = (type) => {
+        return userType === type ? 'active' : ''
+    }
+    const getFormTitle = (type) => {
+        switch (type) {
+            case userTypes.AGENCY:
+                return 'Agency login';
+            case userTypes.EMPLOYER:
+                return 'Employer login';
+            case userTypes.SEEKER:
+                return 'Job Seeker login';
+            default:
+                return ''
+        }
+    }
     return (
-        <div className="c-container pt-3 pb-0">
+        <div className="c-container pt-4 pb-0">
             <div className="login-container">
                 <div className="first-container">
-                    <img src={require('../../assets/images/logo-md.png')} alt="logo" />
+                    <img src={require('../../assets/images/logo-white.png')} alt="logo" />
                     <p>The smartest job site in Middle East.</p>
                 </div>
-                <Form className="second-container" name="normal_login" onFinish={onFinish}>
-                    <h3>User Login</h3>
-                    <Form.Item name="email" className="c-input" rules={Rules.emailRule} >
-                    <label>Email</label>
-                        <Input placeholder="Email" size="large"  type="email" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={Rules.passwordRule}
-                        className="c-password-input mb-2"
-                    >
-                    <label>Password</label>
-                        <Input.Password placeholder="Password" className="c-input" size="large" />
-                    </Form.Item>
-                
-                    <span className="d-flex justify-content-between align-items-center w-50 alt-text">
-                        <Form.Item name="remember" className="mb-0" >
-                            <Checkbox>Remember me</Checkbox>
+                <div className="second-container">
+                    <div className="user-type">
+                        <span
+                            className={`${getIsActive(userTypes.SEEKER)}`}
+                            onClick={() => setUserType(userTypes.SEEKER)}>
+                            Job Seeker
+                        </span>
+                        <span
+                            className={`${getIsActive(userTypes.EMPLOYER)}`}
+                            onClick={() => setUserType(userTypes.EMPLOYER)}>
+                            Employer
+                        </span>
+                        <span
+                            className={`${getIsActive(userTypes.AGENCY)}`}
+                            onClick={() => setUserType(userTypes.AGENCY)}>
+                            Agency
+                        </span>
+                    </div>
+                    <Form className="c-form" onFinish={onFinish}>
+                        <h3 className="form-title">{getFormTitle(userType)}</h3>
+                        <Form.Item name="email" className="c-input" rules={Rules.emailRule} >
+                            <label>Email</label>
+                            <Input placeholder="Email" size="large" type="email" />
                         </Form.Item>
-                        <Link to="/" className="alt-text">Forgot Password</Link>
-                    </span>
-                    <Form.Item>
-                        <CButton
-                            type="large"
-                            htmlType="submit"
-                            // loading={true}
-                            block
+                        <Form.Item
+                            name="password"
+                            rules={Rules.passwordRule}
+                            className="c-password-input mb-2"
                         >
-                            Login
+                            <label>Password</label>
+                            <Input.Password placeholder="Password" className="c-input" size="large" />
+                        </Form.Item>
+
+                        <span className="d-flex justify-content-between align-items-center w-100 alt-text mt-2">
+                            <Form.Item name="remember" className="mb-0" >
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                            <Link to="/" className="alt-text">Forgot Password</Link>
+                        </span>
+                        <Form.Item className="mt-4">
+                            <CButton
+                                type="large"
+                                htmlType="submit"
+                                // loading={true}
+                                block
+                            >
+                                Login
                          </CButton>
-                    </Form.Item>
-                    <Form.Item className="alt-text">
-                        <p>Don't have an account? <Link to="/sign-up"><mark> Sign up</mark></Link></p>
-                    </Form.Item>
-                </Form>
+                        </Form.Item>
+                        <Form.Item className="alt-text mb-0">
+                            <p className="mb-0">Don't have an account? <Link to="/sign-up"><mark> Sign up</mark></Link></p>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         </div>
     )
