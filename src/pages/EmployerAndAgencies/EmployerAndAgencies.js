c32e9e8e25cb3a4a46f39225512f291c5f74b5b0
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-03-02 00:02:41 +0500

    added animation on jobseeker and agency pages

--- src/pages/EmployerAndAgencies/EmployerAndAgencies.js
+++ src/pages/EmployerAndAgencies/EmployerAndAgencies.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import CButton from '../../uiComponents/shared/CButton/CButton'
-
+import Bounce from 'react-reveal/Bounce';
+import Zoom from 'react-reveal/Zoom';
+import Slide from 'react-reveal/Slide';
 function EmployerAndAgencies() {
     return (
         <div className="container">
             <div className="row">
                 <div className="col-lg-8 c-container-centered-content small mb-0 mt-2">
-                    <h2 >
-                        Discover a new way of hiring & make the right connections.
-                     </h2>
+                    <Bounce left >
+                        <h2 >
+                            Discover a new way of hiring & make the right connections.
+                        </h2>
+                    </Bounce>
                 </div>
                 <div className="col-lg-4 c-container-centered-content small mb-0 mt-2">
-                    <CButton
-                        type="large"
-                        htmlType="submit"
-                        // loading={tr`ue}
-                        themeColor="primary"
-                        block
-                    >
-                        Post a free job
-                </CButton>
+                    <Bounce right >
+                        <CButton
+                            type="large"
+                            htmlType="submit"
+                            // loading={tr`ue}
+                            themeColor="primary"
+                            block
+                        >
+                            Post a free job
+                         </CButton>
+                    </Bounce>
                 </div>
             </div>
             <div className="row">
+
                 <div className="col-lg-6 c-container-centered-content  align-items-start">
                     <h4 className="mt-4">
                         Save up to 75% of your <br /> annual recruitment budget
@@ -32,15 +39,18 @@ function EmployerAndAgencies() {
                     </p>
                 </div>
                 <div className="col-lg-5 c-container-centered-content ">
-                    <img className="right-image lg-rad" src={require('../../assets/images/employeeandagency/ea-1.jpg')} alt="ban1" />
-
+                    <Bounce>
+                        <img className="right-image lg-rad" src={require('../../assets/images/employeeandagency/ea-1.jpg')} alt="ban1" />
+                    </Bounce>
                 </div>
             </div>
             <div className="row">
-                <div className="col-lg-8 c-container-centered-content">
-                    <img  className="lg" src={require('../../assets/images/employeeandagency/ea-2.png')} alt="ban1" />
-                    <div className="img-back-cover"></div>
-                </div>
+                <Slide left>
+                    <div className="col-lg-8 c-container-centered-content">
+                        <img className="lg" src={require('../../assets/images/employeeandagency/ea-2.png')} alt="ban1" />
+                        <div className="img-back-cover"></div>
+                    </div>
+                </Slide>
                 <div className="col-lg-4 c-container-centered-content align-items-start">
                     <h4 className="mt-4">
                         Smart profiles
@@ -59,17 +69,20 @@ function EmployerAndAgencies() {
                         Write your interview questions, set a time for each answer, and fire away! Candidates can  then answer each question in a video format,  and once their done it comes back straight into your online inbox.
                     </p>
                 </div>
-                <div className="col-lg-8 c-container-centered-content">
-                    <img className="right-image lg" src={require('../../assets/images/employeeandagency/ea-4.jpg')} alt="ban1" />
+                <Zoom left>
+                    <div className="col-lg-8 c-container-centered-content">
+                        <img className="right-image lg" src={require('../../assets/images/employeeandagency/ea-4.jpg')} alt="ban1" />
 
-                </div>
+                    </div>
+                </Zoom>
             </div>
             <div className="row">
-                <div className="col-lg-8 c-container-centered-content">
-                    <img className="lg" src={require('../../assets/images/employeeandagency/ea-3.jpg')} alt="ban1" />
-                    <div className="img-back-cover"></div>
-
-                </div>
+                <Slide left>
+                    <div className="col-lg-8 c-container-centered-content">
+                        <img className="lg" src={require('../../assets/images/employeeandagency/ea-3.jpg')} alt="ban1" />
+                        <div className="img-back-cover"></div>
+                    </div>
+                </Slide>
                 <div className="col-lg-4 c-container-centered-content align-items-start">
                     <h4 className="mt-4"> Keep your team connected  </h4>
                     <p>
@@ -77,7 +90,7 @@ function EmployerAndAgencies() {
                     </p>
                 </div>
             </div>
-        </div>
+        </div >
 
     )
 }
--- src/pages/JobSeekers/JobSeekers.js
+++ src/pages/JobSeekers/JobSeekers.js
@@ -1,77 +1,88 @@
 import React from 'react'
 import CButton from '../../uiComponents/shared/CButton/CButton'
-
+import Bounce from 'react-reveal/Bounce';
+import Zoom from 'react-reveal/Zoom';
+import Slide from 'react-reveal/Slide';
 function JobSeekers() {
     return (
         <div className="container">
             <div className="row">
                 <div className="col-lg-8 c-container-centered-content align-items-start small mb-0 mt-2">
-                    <h2>
+                    <Bounce left >
+                        <h2>
                             Make the right connections.
                      </h2>
+                    </Bounce>
                 </div>
                 <div className="col-lg-4 c-container-centered-content small mb-0 mt-2">
-                    <CButton
-                        type="large"
-                        htmlType="submit"
-                        // loading={tr`ue}
-                        themeColor="primary"
-                        block
-                    >
-                        Get started
+                    <Bounce right >
+                        <CButton
+                            type="large"
+                            htmlType="submit"
+                            // loading={tr`ue}
+                            themeColor="primary"
+                            block
+                        >
+                            Get started
                 </CButton>
+                    </Bounce>
                 </div>
             </div>
             <div className="row">
                 <div className="col-lg-6 c-container-centered-content  align-items-start">
                     <h4 className="mt-4">
-                    Follow companies
+                        Follow companies
                      </h4>
                     <p>
-                    Follow your favourite companies and stay up to date <br/>with all of their current and future jobs.                    </p>
+                        Follow your favourite companies and stay up to date <br />with all of their current and future jobs.                    </p>
                 </div>
                 <div className="col-lg-5 c-container-centered-content ">
-                    <img className="right-image lg-rad" src={require('../../assets/images/jobseekers/js-1.jpg')} alt="ban1" />
-
+                    <Bounce>
+                        <img className="right-image lg-rad" src={require('../../assets/images/jobseekers/js-1.jpg')} alt="ban1" />
+                    </Bounce>
                 </div>
             </div>
             <div className="row">
-                <div className="col-lg-8 c-container-centered-content">
-                    <img  className="lg" src={require('../../assets/images/jobseekers/js-2.jpg')} alt="ban1" />
-                    <div className="img-back-cover purple"></div>
-                </div>
+                <Slide left>
+                    <div className="col-lg-8 c-container-centered-content">
+                        <img className="lg" src={require('../../assets/images/jobseekers/js-2.jpg')} alt="ban1" />
+                        <div className="img-back-cover purple"></div>
+                    </div>
+                </Slide>
                 <div className="col-lg-4 c-container-centered-content align-items-start">
                     <h4 className="mt-4">
-                    Live chat + inbox
+                        Live chat + inbox
                                          </h4>
                     <p>
-                    Connect with employers and agencies and keep your conversations in one place.                                         </p>
+                        Connect with employers and agencies and keep your conversations in one place.                                         </p>
                 </div>
 
             </div>
             <div className="row">
                 <div className="col-lg-4 c-container-centered-content align-items-start">
                     <h4 className="mt-4">
-                    Smart profile             
+                        Smart profile
                             </h4>
                     <p>
-                    Our smart profiles provide companies with all the info they need to maximize your employment opportunities.                    </p>
+                        Our smart profiles provide companies with all the info they need to maximize your employment opportunities.                    </p>
                 </div>
-                <div className="col-lg-8 c-container-centered-content">
-                    <img className="right-image lg" src={require('../../assets/images/jobseekers/js-3.jpg')} alt="ban1" />
-
-                </div>
+                <Zoom left>
+                    <div className="col-lg-8 c-container-centered-content">
+                        <img className="right-image lg" src={require('../../assets/images/jobseekers/js-3.jpg')} alt="ban1" />
+                    </div>
+                </Zoom>
             </div>
             <div className="row">
-                <div className="col-lg-8 c-container-centered-content">
-                    <img className="lg" src={require('../../assets/images/jobseekers/js-4.jpg')} alt="ban1" />
-                    <div className="img-back-cover purple"></div>
-
-                </div>
+                <Slide left>
+                    <div className="col-lg-8 c-container-centered-content">
+                        <img className="lg" src={require('../../assets/images/jobseekers/js-4.jpg')} alt="ban1" />
+                        <div className="img-back-cover purple"></div>
+                    </div>
+                </Slide>
                 <div className="col-lg-4 c-container-centered-content align-items-start">
                     <h4 className="mt-4"> Smart interview features  </h4>
                     <p>
-                    Interact with companies through our smart video questionnaires, it's almost like you're with them face to face!
+                        Interact with companies through our smart video questionnaires, it's almost like you're with them face to face!
                     </p>
                 </div>
             </div>
