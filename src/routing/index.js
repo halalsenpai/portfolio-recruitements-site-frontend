97a6709ce2cb2e12819aaca2cfeabb3f68118ed0
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-03-02 13:37:18 +0500

    added path recongnition and apply active class on navs

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,6 +1,6 @@
-import React, {Fragment} from 'react'
-import {Route, Switch} from 'react-router-dom';
-import {Home, About} from '../pages';
+import React, { Fragment } from 'react'
+import { Route, Switch } from 'react-router-dom';
+import { Home, About } from '../pages';
 import Login from '../pages/login/Login';
 import SignUp from '../pages/signup/SignUp';
 import AgencySignUp from '../pages/signup/AgencySignUp';
@@ -8,29 +8,32 @@ import EmployerSignUp from '../pages/signup/EmployerSignUp';
 import JobSeekerSignUp from '../pages/signup/JobSeekerSignUp';
 import CFooter from '../uiComponents/CFooter/CFooter';
 import CHeader from '../uiComponents/CHeader/CHeader';
+import { useHistory, useLocation } from 'react-router-dom';
 import EmployerAndAgencies from '../pages/EmployerAndAgencies/EmployerAndAgencies';
 import JobSeekers from '../pages/JobSeekers/JobSeekers';
 function Routing() {
+    const history = useHistory()
+    console.log(history.location.pathname);
     return (
         <Fragment>
-        <CHeader />
+            <CHeader />
             <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/signup" component={SignUp}/>
-                <Route exact path="/user-signup" component={JobSeekerSignUp}/>
-                <Route exact path="/agency-signup" component={AgencySignUp}/>
-                <Route exact path="/employer-signup" component={EmployerSignUp}/>
-                <Route exact path="/signup" component={SignUp}/>
-                <Route exact path="/employee-and-agencies" component={EmployerAndAgencies}/>
-                <Route exact path="/job-seekers" component={JobSeekers}/>
-                <Route path="/about" component={About}/>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route exact path="/user-signup" component={JobSeekerSignUp} />
+                <Route exact path="/agency-signup" component={AgencySignUp} />
+                <Route exact path="/employer-signup" component={EmployerSignUp} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route exact path="/employee-and-agencies" component={EmployerAndAgencies} />
+                <Route exact path="/job-seekers" component={JobSeekers} />
+                <Route path="/about" component={About} />
 
                 <Route path="*" render={() => (
                     <div>
                         <h1>Not Found</h1>
                     </div>
-                )}/>
+                )} />
             </Switch>
             <CFooter />
         </Fragment>
--- src/uiComponents/CHeader/CHeader.js
+++ src/uiComponents/CHeader/CHeader.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import LogoImage from '../../assets/images/logo-md.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LogoImage from '../../assets/images/logo-md.png';
+import { useHistory, useLocation } from 'react-router-dom';
+
 function CHeader() {
+    const history = useHistory()
+    console.log(history.location.pathname);
+    const getActiveClassForPath = (currentLocation, path) => {
+        return currentLocation === path ? 'active b-text' : ''
+    }
+    const { pathname } = history.location;
     return (
         <div className="c-header">
             <span className="inner-container">
                 <Link to="/"><img className="logo" src={LogoImage} alt="Logo" /></Link>
                 <nav className="menu">
-                    <Link to="/">Post a free job</Link>
-                    <Link to="/employee-and-agencies">Employers & agencies</Link>
-                    <Link to="/job-seekers" className="active b-text">Job Seekers</Link>
-                    <Link to="/">Jobs</Link>
+                    <Link to="/" >Post a free job</Link>
+                    <Link 
+                    to="/employee-and-agencies" 
+                    className={getActiveClassForPath(pathname, '/employee-and-agencies')}>
+                        Employers & agencies
+                        </Link>
+                    <Link
+                        to="/job-seekers"
+                        className={getActiveClassForPath(pathname, '/job-seekers')}>
+                        Job Seekers
+                    </Link>
+                    <Link to="/" >Jobs</Link>
                     <Link to="/">Services</Link>
                     <Link to="/">Pricing</Link>
                 </nav>
