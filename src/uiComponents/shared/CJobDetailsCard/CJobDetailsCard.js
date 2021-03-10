f88f001a865b1d39135f82a8f7a1206b94430b48
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-03-10 16:26:58 +0500

    added section in job details

--- src/uiComponents/shared/CJobDetailsCard/CJobDetailsCard.js
+++ src/uiComponents/shared/CJobDetailsCard/CJobDetailsCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CButton from '../../shared/CButton/CButton';
-
+import {FaRegHeart} from 'react-icons/fa'
+import {BsChat} from 'react-icons/bs';
+import {GoStar} from 'react-icons/go';
 function CJobDetailsCard() {
     return (
         <div className="c-job-detail-card">
@@ -19,6 +21,23 @@ function CJobDetailsCard() {
                     </span>
 
                     <CButton themeColor="transparent small">View Jobs</CButton>
+
+                </div>
+
+                <div className="job-details-wrapper">
+
+                    <span className="details-header">
+
+                        <h3 className="job-title">Job title: <mark>Primary Teacher</mark> </h3>
+
+                        <span className="actions-wrapper">
+                            <CButton themeColor="shadowed">Apply</CButton>
+                            <CButton themeColor="shadowed rounded"> <FaRegHeart /> </CButton>
+                            <CButton themeColor="shadowed rounded"> <GoStar/> </CButton>
+                            <CButton themeColor="shadowed rounded"> <BsChat className="highlighted"/> </CButton>
+                        </span>
+
+                    </span>
 
                 </div>
         </div>
