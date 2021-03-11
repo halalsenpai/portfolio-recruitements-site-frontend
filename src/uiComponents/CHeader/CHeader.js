a704ed538574d13fb2637fa50b2a15b38a1b6f98
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-03-11 20:21:39 +0500

    update

--- src/uiComponents/CHeader/CHeader.js
+++ src/uiComponents/CHeader/CHeader.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import LogoImage from '../../assets/images/logo/logo-md.png';
 import { useHistory, useLocation } from 'react-router-dom';
 
 function CHeader() {
-    const history = useHistory()
+    const history = useHistory();
+    const [currentPath,setCurrentPath] = useState('');
+    const { pathname } = history.location;
+    useEffect(() => {
+        setCurrentPath(pathname);
+    }, [pathname]);
     const getActiveClassForPath = (currentLocation, path) => {
-
-        console.log('path ---> ' , currentLocation);
-
-        console.log('path condition',currentLocation === path ? 'active b-text' : '')
 
         return currentLocation === path ? 'active b-text' : ''
 
     }
-    const { pathname } = history.location;
-    console.log('path gettign form history',pathname)
     return (
         <div className="c-header">
             <span className="inner-container">
@@ -24,23 +23,23 @@ function CHeader() {
                     <Link to="/" >Post a free job</Link>
                     <Link
                         to="/employee-and-agencies"
-                        className={getActiveClassForPath(pathname, '/employee-and-agencies')}>
+                        className={getActiveClassForPath(currentPath, '/employee-and-agencies')}>
                         Employers & agencies
                         </Link>
                     <Link
                         to="/job-seekers"
-                        className={getActiveClassForPath(pathname, '/job-seekers')}>
+                        className={getActiveClassForPath(currentPath, '/job-seekers')}>
                         Job Seekers
                     </Link>
                     <Link
                         to="/jobs"
-                        className={getActiveClassForPath(pathname, '/jobs')}>
+                        className={getActiveClassForPath(currentPath, '/jobs')}>
                             Jobs
                             </Link>
                     <Link to="/">Services</Link>
                     <Link
                         to="/pricing"
-                        className={getActiveClassForPath(pathname, '/pricing')}>
+                        className={getActiveClassForPath(currentPath, '/pricing')}>
                         Pricing
                     </Link>
                 </nav>
