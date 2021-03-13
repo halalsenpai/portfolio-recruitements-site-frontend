f0a34b2143a4da87bfa62bfcdb06b73394941594
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2021-03-13 13:21:04 +0500

    handled feature tag through class in job item

--- src/pages/jobs/Data.js
+++ src/pages/jobs/Data.js
@@ -1,12 +1,12 @@
 const data = [
     {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-1.png'), featured:true },
-    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-2.png'), featured:true },
+    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-2.png'), featured:true},
     {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-3.png') , featured:true},
     {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-1.png') },
-    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-2.png') },
-    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-3.png') , featured:true},
+    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-2.png') , },
+    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-3.png') , },
     {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-2.png') },
     {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-1.png') },
-    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-3.png') , featured:true},
+    {title:'Primary', location:'Dubai', company:'Company name', endDate:'Ends in 20 days' ,type:'Full time', salary:'15,000',imgUrl:require('../../assets/images/sample/jobimg-3.png') , },
 ];
 export default data;
--- src/uiComponents/shared/CJobCard/CJobCard.js
+++ src/uiComponents/shared/CJobCard/CJobCard.js
@@ -2,31 +2,50 @@ import React from 'react'
 
 function CJobCard({ job }) {
     return (
-        <div className='c-job-card'>
+        <div className={`c-job-card ${ job.featured && 'featured' } `}>
+
             <span className="info-wrapper">
+
                 <img className="job-thumbnail" src={job.imgUrl} alt="" />
+
                 <span className="info">
+
                     <span className="title-row">
+
                         <h6 className="title">{job.title}</h6>
+                        
                         <span className='location-container'>
+
                             <img className="location-icon" src={require('../../../assets/images/icons/location_icon.svg')} alt="" />
                             <p className="location-text">{job.location}</p>
+
                         </span>
+
                     </span>
+
                     <span>
+
                         <p>{job.company}</p>
                         <p >{job.endDate}</p>
+
                     </span>
                 </span>
             </span>
+
             <span className="more-info">
+
                 <p>{job.type}</p>
                 <p>{job.salary} AED-month</p>
+
             </span>
+
             <span className="tag">
+
                 <img className="tag-icon" src={require('../../../assets/images/icons/diamond_white_icon.svg')} alt="" />
                 <p className="tag-text">Featured</p>
+
             </span>
+
         </div>
     )
 }
