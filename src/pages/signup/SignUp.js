8fca82791593a19aabd7c7b4c2f34600e9ac208d
halalsenpai <abdchaghtai@gmail.com>
2021-04-20 14:07:35 +0500

    ui fixes, header

--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,106 +1,75 @@
-import { Input, Form, Radio } from 'antd';
-import Checkbox from 'antd/lib/checkbox/Checkbox';
-import React from 'react'
-import { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import CButton from '../../uiComponents/shared/CButton/CButton';
-import { userTypes } from '../../utils/constants';
-
+import { Input, Form, Radio } from "antd";
+import Checkbox from "antd/lib/checkbox/Checkbox";
+import React from "react";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import CButton from "../../uiComponents/shared/CButton/CButton";
+import { userTypes } from "../../utils/constants";
 
 function SignUp() {
-    const [role,setRole] = useState(userTypes.SEEKER);
-    const history = useHistory();
-  
-    const onFinish = () =>{
-        
-        switch(role){
-            case userTypes.SEEKER:
-                history.push('user-signup');
-                return;
-            case userTypes.EMPLOYER:
-                history.push('employer-signup');
-                return;
-            case userTypes.AGENCY:
-                history.push('agency-signup');
-                return;
-            default:
-                history.push('user-signup');
-                return;
+  const [role, setRole] = useState(userTypes.SEEKER);
+  const history = useHistory();
 
-        }
+  const onFinish = () => {
+    switch (role) {
+      case userTypes.SEEKER:
+        history.push("user-signup");
+        return;
+      case userTypes.EMPLOYER:
+        history.push("employer-signup");
+        return;
+      case userTypes.AGENCY:
+        history.push("agency-signup");
+        return;
+      default:
+        history.push("user-signup");
+        return;
     }
+  };
 
-     const onChange = e => {
+  const onChange = (e) => {
+    console.log("radio checked", e.target.value);
 
-        console.log('radio checked', e.target.value);
+    setRole(e.target.value);
+  };
 
-       setRole( e.target.value);
-
-      };
-
-    return (
-        <div className="c-container auth-wrapper">
-
-            <div className="signup-container with-form role-container">
-
-                <div className="first-container role-select">
-
-                    <img src={require('../../assets/images/logo/logo-white.png')} alt="logo" />
-
-                </div>
-                <Form className="second-container c-form w-100" onFinish={onFinish}>
-
-                <img src={require('../../assets/images/auth/signup-illus.png')} alt="logo" />
-
-                    <span class="mt-3">
-
-                    <h3 className="form-title">
-                        
-                        <mark>Create an account</mark>
-                        
-                    </h3>
-                    
-                    <Radio.Group onChange={onChange} value={role}>
-
-                        <Radio value={userTypes.SEEKER}>
-                            I'm looking for jobs
-                        </Radio>
-
-                        <Radio value={userTypes.EMPLOYER}>
-                            I'm an employer
-                        </Radio>
-
-                        <Radio value={userTypes.AGENCY}>
-                             I'm a recruiter
-                        </Radio>
-                    </Radio.Group>
-
-                    </span>
-
-                    <Form.Item>
-
-                        <CButton
-                           
-                            type="large"
-                            htmlType="submit"
-                            // loading={true}
-                            block
-                            themeColor='primary'
-                        >
-                            Next
-                        
-                         </CButton>
-
-                    </Form.Item>
-
-                </Form>
-
-            </div>
-
+  return (
+    <div className="c-container auth-wrapper">
+      <div className="signup-container with-form role-container">
+        <div className="first-container role-select">
+          <img className="logo" src={require("../../assets/images/logo/logo-white.png")} alt="logo" />
         </div>
-    )
+        <Form className="second-container c-form w-100" onFinish={onFinish}>
+          <img src={require("../../assets/images/auth/signup-illus.png")} alt="logo" />
 
+          <span class="mt-3">
+            <h3 className="form-title">
+              <mark>Create an account</mark>
+            </h3>
 
+            <Radio.Group onChange={onChange} value={role}>
+              <Radio value={userTypes.SEEKER}>I'm looking for jobs</Radio>
+
+              <Radio value={userTypes.EMPLOYER}>I'm an employer</Radio>
+
+              <Radio value={userTypes.AGENCY}>I'm a recruiter</Radio>
+            </Radio.Group>
+          </span>
+
+          <Form.Item>
+            <CButton
+              type="large"
+              htmlType="submit"
+              // loading={true}
+              block
+              themeColor="primary">
+              Next
+            </CButton>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
--- src/uiComponents/CHeader/CHeader.js
+++ src/uiComponents/CHeader/CHeader.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import LogoImage from "../../assets/images/logo/logo-md.png";
 import { useHistory, useLocation } from "react-router-dom";
-import CButton from "../shared/CButton/CButton";
-import { HiMenuAlt1 } from 'react-icons/hi';
+import { VscChromeClose, VscMenu } from "react-icons/vsc";
 
 function CHeader() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
-  const [menu, setMenu ] = useState(false)
+  const [menu, setMenu] = useState(false);
 
   const getActiveClassForPath = (currentLocation, path) => {
     return currentLocation === path ? "active b-text" : "";
@@ -24,72 +23,73 @@ function CHeader() {
           <Link
             to="/employee-and-agencies"
             onClick={() => setCurrentPath("/employee-and-agencies")}
-            className={getActiveClassForPath(
-              currentPath,
-              "/employee-and-agencies"
-            )}
-          >
+            className={getActiveClassForPath(currentPath, "/employee-and-agencies")}>
             Employers & agencies
           </Link>
           <Link
             to="/job-seekers"
             onClick={() => setCurrentPath("/job-seekers")}
-            className={getActiveClassForPath(currentPath, "/job-seekers")}
-          >
+            className={getActiveClassForPath(currentPath, "/job-seekers")}>
             Job Seekers
           </Link>
           <Link
             to="/jobs"
             onClick={() => setCurrentPath("/jobs")}
-            className={getActiveClassForPath(currentPath, "/jobs")}
-          >
+            className={getActiveClassForPath(currentPath, "/jobs")}>
             Jobs
           </Link>
           <Link to="/">Services</Link>
           <Link
             to="/pricing"
             onClick={() => setCurrentPath("/pricing")}
-            className={getActiveClassForPath(currentPath, "/pricing")}
-          >
+            className={getActiveClassForPath(currentPath, "/pricing")}>
             Pricing
           </Link>
         </nav>
-        <div className={`mobile-menu ${ menu ? "menu-open" : ""}`}>
+        <div className={`mobile-menu ${menu ? "menu-open" : ""}`}>
           <Link
-           to="/"
-           onClick={() => {setCurrentPath("/"); setMenu(false)}}
-
-        >Post a free job</Link>
+            to="/"
+            onClick={() => {
+              setCurrentPath("/");
+              setMenu(false);
+            }}>
+            Post a free job
+          </Link>
           <Link
             to="/employee-and-agencies"
-            onClick={() => {setCurrentPath("/employee-and-agencies"); setMenu(false)}}
-            className={getActiveClassForPath(
-              currentPath,
-              "/employee-and-agencies"
-            )}
-          >
+            onClick={() => {
+              setCurrentPath("/employee-and-agencies");
+              setMenu(false);
+            }}
+            className={getActiveClassForPath(currentPath, "/employee-and-agencies")}>
             Employers & agencies
           </Link>
           <Link
             to="/job-seekers"
-            onClick={() => {setCurrentPath("/job-seekers"); setMenu(false)}}
-            className={getActiveClassForPath(currentPath, "/job-seekers")}
-          >
+            onClick={() => {
+              setCurrentPath("/job-seekers");
+              setMenu(false);
+            }}
+            className={getActiveClassForPath(currentPath, "/job-seekers")}>
             Job Seekers
           </Link>
           <Link
             to="/jobs"
-            onClick={() => {setCurrentPath("/jobs"); setMenu(false)}}
-            className={getActiveClassForPath(currentPath, "/jobs")}
-          >
+            onClick={() => {
+              setCurrentPath("/jobs");
+              setMenu(false);
+            }}
+            className={getActiveClassForPath(currentPath, "/jobs")}>
             Jobs
           </Link>
           <Link to="/">Services</Link>
           <Link
             to="/pricing"
-            onClick={() => {setCurrentPath("/pricing"); setMenu(false)}}
-            className={getActiveClassForPath(currentPath, "/pricing")}
-          >
+            onClick={() => {
+              setCurrentPath("/pricing");
+              setMenu(false);
+            }}
+            className={getActiveClassForPath(currentPath, "/pricing")}>
             Pricing
           </Link>
         </div>
@@ -100,8 +100,8 @@ function CHeader() {
           <Link className="text-decoration-none" to="/signup">
             <button className="c-button blue sign-up-btn">Sign Up</button>
           </Link>
-          <button onClick={()=>setMenu(!menu)} className="toggle-button">
-              <HiMenuAlt1 size="25px"/>
+          <button onClick={() => setMenu(!menu)} className="toggle-button">
+            {menu ? <VscChromeClose size="25px" /> : <VscMenu size="25px" />}
           </button>
         </nav>
       </span>
