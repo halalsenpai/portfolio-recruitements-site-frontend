88befb03c863528ae79a476339a154ecb2e5a951
shoaib8236 <70111644+shoaib8236@users.noreply.github.com>
2021-06-05 12:45:18 +0500

    HomePage: Fixes Done Matched Sizes With XD Desing

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -36,7 +36,7 @@ function Home() {
                 </Fade>
 
                 <Fade bottom delay={500}>
-                  <p className="banner-content mobile-textalign pr-4 mb-4">
+                  <p className="banner-content mobile-textalign">
                     We asked 97 companies, and 1000 job seekers what they
                     struggle with when searching on job sites, and we got the
                     answers, so we built the smartest and the most advanced
@@ -49,7 +49,7 @@ function Home() {
                   <div>
                     <Button
                       onClick={() => history.push("/signup")}
-                      className="mt-4 button-center get-started-btn"
+                      className="mt-4 mb-3 button-center get-started-btn"
                       themeColor="blue"
                       type={"large"}>
                       Get Started{" "}
@@ -79,7 +79,7 @@ function Home() {
 
         <Fade bottom duration={1200}>
           <div>
-            <div className="c-container-centered-content mb-2 medium">
+            <div className="home-content-container">
               <h2 className="main-heading">EMPLOYERS, AGENCIES, JOBSEEKERS,</h2>
               <h5 className="mobile-textalign">connected in one platform.</h5>
             </div>
@@ -93,64 +93,60 @@ function Home() {
             </div>
           </div>
         </Fade>
+        <div className="container-benefit">
+          <Fade bottom duration={1200}>
+            <div className="inner-container">
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ea/ea-1.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="a-text">Free CRM</h3>
+                  <p>
+                    Stay connected with your <br /> entire team.
+                  </p>
+                </span>
+              </div>
 
-        <div className="container">
-          <div className="c-container-centered-content mt-0 mb-0">
-            <div className="ea-container">
-              {/* <h4 className="b-text">Employers & Agencies</h4> */}
-
-              <span className="inner-container">
-                <Fade bottom duration={1200}>
-                  <div className="inner-container">
-                    <div className="box">
-                      <img
-                        src={require("../../assets/images/icons/ea/ea-1.svg")}
-                        alt="img"
-                      />
-                      <span>
-                        <h3 className="a-text">Free CRM</h3>
-                        <p>Built in CRM with drag and Drop function</p>
-                      </span>
-                    </div>
-
-                    <div className="box">
-                      <img
-                        src={require("../../assets/images/icons/ea/ea-2.svg")}
-                        alt="img"
-                      />
-                      <span>
-                        <h3 className="a-text">Save up to 75%</h3>
-                        <p>Save up to 75% of your annual recruitment budget</p>
-                      </span>
-                    </div>
-                    <div className="box">
-                      <img
-                        src={require("../../assets/images/icons/ea/ea-3.svg")}
-                        alt="img"
-                      />
-                      <span>
-                        <h3 className="a-text">Direct chat + Inbox</h3>
-                        <p>Connect with candidates direct, no more emails!</p>
-                      </span>
-                    </div>
-                    <div className="box">
-                      <img
-                        src={require("../../assets/images/icons/ea/ea-4.svg")}
-                        alt="img"
-                      />
-                      <span>
-                        <h3 className="a-text">Candidate Match</h3>
-                        <p>
-                          Set accurate filters and let the system find you job
-                          seekers!
-                        </p>
-                      </span>
-                    </div>
-                  </div>
-                </Fade>
-              </span>
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ea/ea-2.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="a-text">Save up to 75%</h3>
+                  <p>
+                    Save up to 75% of <br /> your annual recruitment budget
+                  </p>
+                </span>
+              </div>
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ea/ea-3.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="a-text">Direct chat + Inbox</h3>
+                  <p>
+                    Keep all your conversations in <br /> one place.
+                  </p>
+                </span>
+              </div>
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ea/ea-4.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="a-text">Candidate Match</h3>
+                  <p>
+                    Find the suitable person for the <br /> job in a few clicks.
+                  </p>
+                </span>
+              </div>
             </div>
-          </div>
+          </Fade>
         </div>
         <div className="space"></div>
         <Fade bottom duration={1200}>
@@ -173,75 +169,66 @@ function Home() {
           </div>
         </Fade>
         <div className="space"></div>
-        <div className="container">
-          <div className="c-container-centered-content mt-0">
-            <div className="ea-container primary mt-0">
-              {/* <h4 className="b-text">Looking for a new job?</h4> */}
-              <Fade bottom duration={1200}>
-                <span className="inner-container">
-                  <div className="box">
-                    <img
-                      src={require("../../assets/images/icons/ma/ma-1.svg")}
-                      alt="img"
-                    />
-                    <span>
-                      <h3 className="b-text">One click apply</h3>
-                      <p>
-                        Shortlist jobs and apply to all of them with 1 click
-                      </p>
-                    </span>
-                  </div>
-
-                  <div className="box">
-                    <img
-                      src={require("../../assets/images/icons/ma/ma-2.svg")}
-                      alt="img"
-                    />
-                    <span>
-                      <h3 className="b-text">Job Match</h3>
-                      <p>
-                        Let our system do the work for you even while you sleep!
-                      </p>
-                    </span>
-                  </div>
-                  <div className="box">
-                    <img
-                      src={require("../../assets/images/icons/ma/ma-3.svg")}
-                      alt="img"
-                    />
-                    <span>
-                      <h3 className="b-text">Direct chat + Inbox</h3>
-                      <p>
-                        Talk to employers & agencies in real time, no emails!
-                      </p>
-                    </span>
-                  </div>
-
-                  <div className="box">
-                    <img
-                      src={require("../../assets/images/icons/ma/ma-4.svg")}
-                      alt="img"
-                    />
-                    <span>
-                      <h3 className="b-text">Follow Companies</h3>
-                      <p>
-                        Follow companies and stay up to date with all their jobs
-                      </p>
-                    </span>
-                  </div>
+        <div className="container-filters">
+          <Fade bottom duration={1200}>
+            <span className="inner-container">
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ma/ma-1.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="b-text">One click apply</h3>
+                  <p>Shortlist jobs and apply to all of them with 1 click</p>
                 </span>
-              </Fade>
-            </div>
-          </div>
+              </div>
+
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ma/ma-2.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="b-text">Job Match</h3>
+                  <p>
+                    Let our system do the work for you even while you sleep!
+                  </p>
+                </span>
+              </div>
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ma/ma-3.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="b-text">Direct chat + Inbox</h3>
+                  <p>Talk to employers & agencies in real time, no emails!</p>
+                </span>
+              </div>
+              <div className="box">
+                <img
+                  src={require("../../assets/images/icons/ma/ma-4.svg")}
+                  alt="img"
+                />
+                <span>
+                  <h3 className="b-text">Follow Companies</h3>
+                  <p>
+                    Follow companies and stay up to date with all their jobs
+                  </p>
+                </span>
+              </div>
+            </span>
+          </Fade>
         </div>
-
-        <div className="space"></div>
-
         <div className="container-fluid bottom-container">
           {/* <Fade bottom duration={1000}> */}
           <div className="row">
-            <div className="padding-mobile col-lg-5 text-column pb-5 ">
-              {/* <img className="bottom-logo" src={require("..//..//assets/images/logo/logo-md.png")} alt="" /> */}
+            <div className="padding-mobile bottom-text-section col-lg-4 text-column ">
+              <img
+                className="bottom-logo"
+                src={require("..//..//assets/images/logo/logo-md.png")}
+                alt=""
+              />
 
               <span className="small-paragraph">
                 <p>
@@ -269,7 +256,7 @@ function Home() {
                   sectors.
                 </p>
               </span>
-              <Button className="mt-2" themeColor="blue">
+              <Button className="demo-btn" themeColor="blue">
                 <a
                   className="book-a-demo-btn"
                   href="https://calendly.com/jobsmideast/demo?month=2021-04&back=1">
@@ -278,7 +265,7 @@ function Home() {
               </Button>
             </div>
 
-            <div className="col-lg-7 p-0 d-flex align-items-center">
+            <div className="col-lg-8 p-0 d-flex align-items-center">
               <div className="container-fluid p-0 d-flex justify-content-center">
                 <img
                   className="w-100 safari-fix"
